e275216cd1468bc92d322393e6bbfafbdba18331
Kamdev Kumar <87974605+kamdev66@users.noreply.github.com>
2022-01-12 21:18:52 +0530

    Update Subarray with given sum.cpp

--- C++ Solution/Arrays/Subarray with given sum.cpp
+++ C++ Solution/Arrays/Subarray with given sum.cpp
@@ -27,3 +27,27 @@ int i,j;
 }
 
 /* Method:-2 by using Two pointer method , Time:- O(n), Space:- O(1) */
+#include<iostream>
+using namespace std;
+int main(){
+	int n,s;
+	cin>>n>>s;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	int st=0,sum=0,i;
+	for(i=0;i<n;i++){
+		while(sum>s && st<i){
+			sum=sum-a[st];
+			st++;  
+		}
+		if(sum==s){
+			cout<<st<<" "<<i-1<<endl;
+		}
+		if(i<n){
+			sum=sum+a[i];
+		}
+	}
+	
+}
